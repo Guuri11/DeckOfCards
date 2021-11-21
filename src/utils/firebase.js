6edeb7b603701357f338fab677bad3fd63b28544
Sergio Gurillo Corral <sergio.gurillo11@gmail.com>
2021-11-21 20:22:38 +0100

    firebase setup

---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
